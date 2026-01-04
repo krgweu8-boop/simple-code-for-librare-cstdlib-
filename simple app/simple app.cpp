c02f3c91b0e0a0fb9d9a6553e6ba918f8a3c1d86
krgwe <krgwe@DESKTOP-BANIPD6>
2026-01-05 01:51:24 +0300

    Добавьте файлы проекта.

---
+++ simple app/simple app.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main() {
+
+	srand(time(0));
+
+	int randomNum[5]{};
+
+	for (int i = 0; i < 5; i++) {
+		randomNum[i] = rand() % 100 + 1;
+		std::cout << randomNum[i] << " ";
+	}
+	std::cout << std::endl;
+	return 0;
+}
